14a7a858851bb9141e80230da6ffd1a32b8e8f87
Kyle <kderrick1988@gmail.com>
2016-04-01 10:09:48 -0700

    add ability to display 2 models on index

---
+++ app/components/new-answer.js
@@ -0,0 +1,18 @@
+// import Ember from 'ember';
+//
+// export default Ember.Component.extend({
+//   addNewAnswer: false,
+//   actions: {
+//     answerFormShow() {
+//       this.set('addNewAnswer', true);
+//     },
+//     save() {
+//       var params = {
+//         title: this.get('title'),
+//         author: this.get('author')
+//       };
+//       this.set('addNewAnswer', false);
+//       this.sendAction('save', params);
+//     }
+//   }
+// });
