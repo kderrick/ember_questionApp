89a5c5a333c18f7d95648bfd0f0c4eb4c271f2cd
Kyle <kderrick1988@gmail.com>
2016-04-01 08:00:26 -0700

    link firebase to index to display questions.json file

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'question-app',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://kderrick1988questionapp.firebaseio.com//',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
