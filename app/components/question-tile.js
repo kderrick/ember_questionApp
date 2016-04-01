25c45b3b26d72ebd6710504096627ce85660f108
Kyle <kderrick1988@gmail.com>
2016-04-01 08:58:50 -0700

    add update functionality for question on index page

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    update(question,params) {
+      this.sendAction('update', question, params);
+    },
     delete(question) {
       if(confirm("Would you like to delete this question?")) {
         this.sendAction('destroyQuestion', question);
--- app/routes/index.js
+++ app/routes/index.js
@@ -9,11 +9,20 @@ export default Ember.Route.extend({
       question.destroyRecord();
       this.transitionTo('index');
     },
+    update(question, params) {
+      debugger;
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
     save(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-
     }
   }
 });
