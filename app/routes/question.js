ab1248c8c44393504d4eba2d4b4eee730abba4db
Kyle Derrick <kderrick1988@gmail.com>
2016-04-08 08:47:17 -0700

    update deleteQuestion method to also delete all associated answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -24,8 +24,13 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
     destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
+      var answer_deletions = question.get('answers').map(function(answer){
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function(){
+        return question.destroyRecord();
+      });
+       this.transitionTo('index');
     },
     deleteAnswer(answer) {
       answer.destroyRecord();
