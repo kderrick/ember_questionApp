0f7d35a5ba812e793aeef0754a029ff866f9ee0f
Kyle <kderrick1988@gmail.com>
2016-04-12 20:34:34 -0700

    add computed property for displaying full question details

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullQuestionInfo: Ember.computed('question.title', 'question.author','question.details', function() {
+    return this.get('question.title') + 'by: ' + this.get('question.author') + 'the deets' + this.get('question.details');
+  }),
   actions: {
     delete(question) {
       if(confirm("Would you like to delete this question?")) {
